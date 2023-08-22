97ad64247b90aa988aed6492f9d64b0b427b510f
spribojac <sophie170995@live.co.uk>
2023-08-22 22:42:56 +0100

    Updated comments for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,24 +16,27 @@ with open(csvpath) as csvfile:
     # Read the header row first
     budget_header = next(budget_data)
 
-
     # Initialise variables
     total_months = 0
     total_profit = 0
-    revenue_changes = []
     previous_month_revenue = 0
     greatest_increase = 0
     greatest_increase_month = ""
     greatest_decrease = 0 
     greatest_decrease_month = ""
 
+    # Initialise a list for the revenue changes
+    # This is so I can store each revenue change in a list to be used later when calculating the greatest increase and decrease in profits
+    revenue_changes = []
+
+    # For each row in the csvfile
     for row in budget_data:
 
-        # Total number of months
+        # Total the number of months
         months = row[0]
         total_months += 1 
         
-        # Net total amount of Profit for the month
+        # Calculate the net total amount of profit for the month
         profit = int(row[1])
         total_profit += profit
 
@@ -43,7 +46,8 @@ with open(csvpath) as csvfile:
             revenue_change = current_month_revenue - previous_month_revenue
             revenue_changes.append(revenue_change)
 
-            # Check if the revenue change is larger than the previously stored greatest increase
+            # Within this loop...
+            # Check if the calculated revenue change is larger than the previously stored greatest increase
             if revenue_change > greatest_increase:
                 greatest_increase = revenue_change
                 # Store the month
@@ -54,16 +58,19 @@ with open(csvpath) as csvfile:
                 greatest_decrease = revenue_change
                 # Store the month
                 greatest_decrease_month = row[0]
-
-        # Update the previous month revenue with the current month revenue
+        
+        # Now I have used the new revenue change value I can...
+        # Update the previous month revenue with the current month revenue for the next iteration
         previous_month_revenue = current_month_revenue
 
     # Calculate the average change in profit/loss over the entire period
+    # This is at the end of the FOR loop because I need the full list of revenue changes for this calculation
     average_change = sum(revenue_changes)/len(revenue_changes)
 
-    # Print the financial analysis
+# Store the string outputs of all analyses in a variable called output
+# This is done so I can easily print into the text file below without having to repeat the lines of code for the strings.
 output = (
-   f"Financial Analysis\n"
+    f"Financial Analysis\n"
     f"------------------------------\n"
     f"Total Months: {total_months}\n"
     f"Total: ${total_profit}\n"
@@ -72,6 +79,7 @@ output = (
     f"Greatest Decrease in Profits: ${greatest_decrease} ({greatest_decrease_month})\n"
     )
 
+# Print the financial analysis
 print(output)
 
 # Export results as a text file
